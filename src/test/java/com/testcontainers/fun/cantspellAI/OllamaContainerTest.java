e674858fe42e145dc76d6411dd909a2a1da9dcf2
Samir Akarioh <sakarioh@gatling.io>
2024-09-11 10:23:21 +0200

    s

--- src/test/java/com/testcontainers/fun/cantspellAI/OllamaContainerTest.java
+++ src/test/java/com/testcontainers/fun/cantspellAI/OllamaContainerTest.java
@@ -1,29 +1,45 @@
 package com.testcontainers.fun.cantspellAI;
 
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
 
+import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.ollama.OllamaContainer;
-
-import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class OllamaContainerTest {
+public class OllamaContainerTest extends Simulation {
 
     @Test
     public void withDefaultConfig() {
-        try ( // container {
-              OllamaContainer ollama = new OllamaContainer("ollama/ollama:0.1.40")
-              // }
-        ) {
+        try (OllamaContainer ollama = new OllamaContainer("ollama/ollama:0.1.40")) {
             ollama.start();
 
-            String version = given()
+            String version = RestAssured.given()
                     .baseUri(ollama.getEndpoint())
                     .get("/api/version")
                     .jsonPath()
                     .get("version");
 
             assertThat(version).isEqualTo("0.1.40");
+
+            HttpProtocolBuilder httpProtocol =
+                http.baseUrl("https://computer-database.gatling.io")
+                .acceptHeader("application/json")
+                .contentTypeHeader("application/json");
+
+
+                
+            ScenarioBuilder myFirstScenario = scenario("My First Scenario")
+                .exec(http("Request 1").get("/computers/"));
+
+            setUp(
+                myFirstScenario.injectOpen(constantUsersPerSec(2).during(60))
+            ).protocols(httpProtocol);
         }
     }
 }
